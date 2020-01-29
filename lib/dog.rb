95be335e50f77f0aa2e9a9ccdda5c1271a7f7d67
Roberto Livi <robertomlivi@outlook.com>
2020-01-29 22:03:05 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -10,7 +10,7 @@ class Dog
   end
   
   def name
-    puts "#{@name}"
+    "#{@name}"
   end
   
   def breed=(breed)
@@ -18,7 +18,7 @@ class Dog
   end
   
   def breed
-   puts "#{@breed}"
+   "#{@breed}"
   end
 end
 
